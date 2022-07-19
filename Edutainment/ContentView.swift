dfe1dd03710798e3b751a67d1009b0655481d671
Milosz Tabaka <miloszmtabaka@gmail.com>
2022-07-19 23:55:13 +0100

    Settings layout completed

--- Edutainment/ContentView.swift
+++ Edutainment/ContentView.swift
@@ -8,9 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var table: Int = 5
+    @State private var questionsSelection: Int = 5
+    
+    let questions = [5, 10, 15, 20]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    Stepper("\(table)", value: $table, in: 2...12)
+                } header: {
+                    Text("Tables up to")
+                }
+                Section {
+                    Picker("Number of Questions", selection: $questionsSelection) {
+                        ForEach(questions, id: \.self) { num in
+                            Text("\(num)")
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("Number of Questions")
+                }
+            
+            
+                NavigationLink(destination: Gameplay(table: table, numOfQuestions: questionsSelection)) {
+                    Image(systemName: "play.fill")
+                        .font(.title)
+                        .foregroundColor(.blue)
+                        .padding()
+                }
+            }
+            .navigationTitle("Multiplication")
+        }
+        
     }
 }
 
